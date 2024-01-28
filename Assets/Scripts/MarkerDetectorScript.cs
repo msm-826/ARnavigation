024ba330a09ff28cd957a9ab652ffa72a7a3337f
Muhammed Danish P P <danishmuhammed01@gmail.com>
2024-01-28 15:32:16 +0530

    modifier image detection script and scene
    now every time retry button is clicked AR session is restarted. this is done because when arfoundation detects one image and switches to another image  , it cant detect the first image again.
    
    AR Foundation v5.1.1
    There is no API to determine the visibility of an image. Generally, if the tracking state is Tracking, it will likely change to Limited when the image is not visible. However, there are other situations in which the tracking state can be in states other than Tracked

--- Assets/Scripts/MarkerDetectorScript.cs
+++ Assets/Scripts/MarkerDetectorScript.cs
@@ -1,25 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
 
 public class MarkerDetectorScript : MonoBehaviour {
     [SerializeField]
-    private ARTrackedImageManager imageManager;
+    private ARTrackedImageManager trackedImageManager;
 
     [SerializeField]
     TextMeshProUGUI locationName;
 
-    // Update is called once per frame
-    void Update () {
-        foreach (var trackedImage in imageManager.trackables) {
-            if (trackedImage.trackingState == TrackingState.Tracking) {
-                locationName.text = trackedImage.referenceImage.name;
-            } else {
-                locationName.text = string.Empty;
-            }
+    [SerializeField]
+    Button retryButton;
+
+    [SerializeField]
+    private ARSession arSession;
+
+
+    private bool isDetecting = true;
+
+    private void Awake () {
+        Debug.Log("Awake: Setting up event listeners");
+        trackedImageManager.trackedImagesChanged += OnChanged;
+        retryButton.onClick.AddListener(() => {
+            Debug.Log("Retry button clicked: Restarting AR session");
+            arSession.Reset();
+            locationName.text = string.Empty;
+            isDetecting = true;
+        });
+        Debug.Log("Awake: MarkerDetectorScript enabled");
+    }
+
+    private void OnDisable () {
+        Debug.Log("OnDisable: Removing event listeners");
+        trackedImageManager.trackedImagesChanged -= OnChanged;
+        Debug.Log("OnDisable: MarkerDetectorScript disabled");
+    }
+
+    private void OnChanged (ARTrackedImagesChangedEventArgs eventArgs) {
+        Debug.Log($"OnChanged: Detecting images: {isDetecting}");
+        if (!isDetecting) return;
+
+        foreach (var newImage in eventArgs.added) {
+            Debug.Log($"OnChanged: New image detected: {newImage.referenceImage.name}");
+            locationName.text = newImage.referenceImage.name;
+            isDetecting = false;
+            Debug.Log("OnChanged: Stopped detection");
         }
     }
 }
