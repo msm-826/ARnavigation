efe69d9970897815e68f5055a41fc9b49595a947
Muhammed Danish P P <danishmuhammed01@gmail.com>
2024-01-31 20:11:58 +0530

    AR session reset on exit

--- Assets/Scripts/MarkerDetectorScript.cs
+++ Assets/Scripts/MarkerDetectorScript.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 
@@ -19,33 +20,30 @@ public class MarkerDetectorScript : MonoBehaviour {
 
     private bool isDetecting = true;
 
-    private void Awake () {
-        Debug.Log("Awake: Setting up event listeners");
+    private void OnEnable() {
         trackedImageManager.trackedImagesChanged += OnChanged;
-        retryButton.onClick.AddListener(() => {
-            Debug.Log("Retry button clicked: Restarting AR session");
-            arSession.Reset();
-            locationName.text = string.Empty;
-            isDetecting = true;
-        });
-        Debug.Log("Awake: MarkerDetectorScript enabled");
     }
 
     private void OnDisable () {
-        Debug.Log("OnDisable: Removing event listeners");
         trackedImageManager.trackedImagesChanged -= OnChanged;
-        Debug.Log("OnDisable: MarkerDetectorScript disabled");
     }
 
     private void OnChanged (ARTrackedImagesChangedEventArgs eventArgs) {
-        Debug.Log($"OnChanged: Detecting images: {isDetecting}");
         if (!isDetecting) return;
 
         foreach (var newImage in eventArgs.added) {
-            Debug.Log($"OnChanged: New image detected: {newImage.referenceImage.name}");
             locationName.text = newImage.referenceImage.name;
             isDetecting = false;
-            Debug.Log("OnChanged: Stopped detection");
         }
+    }
+
+    public void OnARSessionExit (int SceneIndex) {
+        SceneManager.LoadScene(SceneIndex, LoadSceneMode.Single);
+    }
+
+    public void onRetryClick () { 
+        arSession.Reset();
+        locationName.text = string.Empty;
+        isDetecting = true;
     }
 }
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,14 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour {
 
     public void LoadScene(int SceneIndex) { 
-        SceneManager.LoadScene(SceneIndex);
+        SceneManager.LoadScene(SceneIndex, LoadSceneMode.Single);
     }
-
+    
     public void QuitApplication () { 
         Application.Quit();
     }
